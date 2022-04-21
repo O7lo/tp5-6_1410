ce76b113d3b0ddeb56db6627aa8c1feb1744edd1
O7lo <80862226+O7lo@users.noreply.github.com>
2022-04-21 16:35:08 -0400

    unitproduct

--- Recipes/UnitProduct.cpp
+++ Recipes/UnitProduct.cpp
@@ -9,8 +9,7 @@ UnitProduct::UnitProduct(std::string name, std::string origin, int quantity, flo
 
 AbsCatalogComponent * UnitProduct::clone(void) const 
 { 
-	// À compléter pour construire un nouvel objet UnitProduct en appelant le constructeur de copie
-	return nullptr; // À remplacer
+	return new UnitProduct(*this);
 }
 
 void UnitProduct::setQuantityPrice(int quantity, float price)
@@ -31,6 +30,7 @@ int UnitProduct::getQuantity() const
 
 std::ostream & UnitProduct::printToStream(std::ostream & o) const
 {
-	// À compléter pour imprimer sur un stream un produit vendu à l'unité
+	o << m_name << "(from " << this->getOrigin() << ") " << m_price
+		<< "/pc." << std::endl;
 	return o;
 }
